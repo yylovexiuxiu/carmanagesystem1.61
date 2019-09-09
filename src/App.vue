899edd7a36da946abbbe9818de7ba0fb4d75a7da
liuyingying <o481@qq.com>
2019-09-09 14:09:53 +0800

    123

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,34 @@
 <template>
-  <div id="app" class="app-body">
-    <!--顶部header区域-->
-    <header id="header" class="mui-bar mui-bar-nav">
-      <h1 class="mui-title">车在哪</h1>
-      <button class="mui-action-back mui-btn mui-btn-blue mui-btn-link mui-btn-nav mui-pull-left">
-        <span class="mui-icon mui-icon-left-nav"></span>首页
-      </button>
-      <a class="mui-icon mui-icon-bars mui-pull-right"></a>
-    </header>
-    <!--中间router-view-->
-    <router-view>
-    </router-view>
-    <!--底部tabBar区域-->
-    <nav class="mui-bar mui-bar-tab">
-      <router-link class="mui-tab-item" to="/map">
-        <span class="mui-icon mui-icon-map"></span>
-        <span class="mui-tab-label">地图</span>
-      </router-link>
-      <router-link class="mui-tab-item" to="/device">
-        <span class="iconfont icon-shebei1"></span>
-        <span class="mui-tab-label">设备</span>
-      </router-link>
-      <router-link class="mui-tab-item" to="/message">
-        <span class="mui-icon mui-icon-email"><span class="mui-badge">9</span></span>
-        <span class="mui-tab-label">消息</span>
-      </router-link>
-      <router-link class="mui-tab-item" to="/user">
-        <span class="mui-icon mui-icon-contact"></span>
-        <span class="mui-tab-label">我的</span>
-      </router-link>
-    </nav>
-  </div>
+    <div class="app-body" id="app">
+        <!--router-view-->
+        <router-view>
+        </router-view>
+    </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    //HelloWorld
-  }
-}
+    export default {
+        name: 'app',
+        components: {},
+        method: {},
+        ready: function () {
+            console.log("APP is ready");
+        },
+        created: function () {
+            console.log("APP is created");
+            this.$router.push('/login');
+        },
+        mounted: function () {
+            console.log("APP is mounted");
+        }
+    }
 </script>
 
-<style>
-  .app-body {
-    width: 100%;
-    height: 100%;
-    padding-top: 44px;
-    padding-bottom: 53px;
-  }
+<style lang='less' scoped>
+    .app-body {
+        width: 100%;
+        height: 100%;
+        padding-top: 0px;
+        padding-bottom: 0px;
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -4,11 +4,20 @@ import VueResource from 'vue-resource'
 import VueRouter from 'vue-router'
 import BaiduMap from 'vue-baidu-map'
 import {BmlLushu} from 'vue-baidu-map'
+//import './assets/css/weui.min.css'
+import './assets/css/app.css'
+import './assets/css/weui.css'
+import './assets/css/weuix.css'
 //倒入router.js模块
 import router from './router.js'
+// //导入weui.js中的文件
+//import weuijs '../assets/js/zepto.min.js'
+import weui from 'weui.js'
+//import 'weui'
+Vue.prototype.$weui = weui
 //使用百度地图
 Vue.use(BaiduMap, {
-  ak: 's7mAXGzAueWSfZdOoq2ToIg1HMTNjD6G'
+    ak: 's7mAXGzAueWSfZdOoq2ToIg1HMTNjD6G'
 })
 //注册路书组件
 Vue.component('bml-lushu', BmlLushu)
@@ -20,7 +29,7 @@ Vue.use(VueRouter)
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
-  //挂载router对象到vue实例上
-  router
+    render: h => h(App),
+    //挂载router对象到vue实例上
+    router
 }).$mount('#app')
